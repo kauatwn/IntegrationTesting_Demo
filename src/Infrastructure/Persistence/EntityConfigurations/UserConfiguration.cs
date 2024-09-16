8dd0d3a361113e3cbf1bc2b256410bab58279597
kauatwn <kaualopescontato@gmail.com>
2024-09-16 17:47:16 -0300

    Criando UserConfiguration

---
+++ src/Infrastructure/Persistence/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.EntityConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.CreatedAt).HasDefaultValue(DateTime.UtcNow).IsRequired();
+
+        builder.Property(u => u.Email).HasMaxLength(256).IsRequired();
+        builder.HasIndex(u => u.Email).IsUnique();
+    }
+}
