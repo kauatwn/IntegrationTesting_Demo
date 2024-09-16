c41d1ef4ccde46e3eeaa63987947e81abd8fddda
kauatwn <kaualopescontato@gmail.com>
2024-09-16 17:47:01 -0300

    Criando UnitOfWork

---
+++ src/Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Persistence.Context;
+
+namespace Infrastructure.Persistence;
+
+public class UnitOfWork(AppDbContext dbContext) : IUnitOfWork
+{
+    private AppDbContext DbContext { get; } = dbContext;
+
+    public async Task CommitAsync()
+    {
+        await DbContext.SaveChangesAsync();
+    }
+}
