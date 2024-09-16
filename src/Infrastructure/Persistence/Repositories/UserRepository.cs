2bb03fd641d05ec591d23171d1e5875aa12ca5f5
kauatwn <kaualopescontato@gmail.com>
2024-09-16 17:46:45 -0300

    Criando UserRepository

---
+++ src/Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using Infrastructure.Persistence.Context;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class UserRepository(AppDbContext dbContext) : IUserWriteOnlyRepository
+{
+    private AppDbContext DbContext { get; } = dbContext;
+
+    public async Task AddAsync(User user)
+    {
+        await DbContext.Users.AddAsync(user);
+    }
+}
