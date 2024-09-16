5bc00ca25a034b941aa99ecac33f6d48e9f880e2
kauatwn <kaualopescontato@gmail.com>
2024-09-16 17:35:22 -0300

    Criando entidade User

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class User(string name, string email)
+{
+    public int Id { get; private set; } 
+    public string Name { get; private set; } = name;
+    public string Email { get; private set; } = email;
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+}
